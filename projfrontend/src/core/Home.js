3468b91b3b54194ebba38c61bc9ac04192595a49
hakeemullahjan <h.jan210@gmail.com>
2020-07-02 21:11:47 +0500

    14 Navigation and Authentication

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -7,7 +7,7 @@ import { API } from "../backend";
 const Home = () => {
   console.log("API IS", API);
   return (
-    <Base title="Title Page">
+    <Base title="Title Page" description="Welcome to the T-shirt Store">
       <div className="row">
         <div className="col-4">
           <button className="btn btn-success">TEST</button>
